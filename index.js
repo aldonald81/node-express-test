2add6edfe92a3bda3cb80367678c3a58d81c45a1
Alexander <aldonald@davidson.edu>
2023-08-19 20:02:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 
 const app = express()
 
-app.listen(8080, () => console.log("API running on port 5001"))
+app.listen(8080, () => console.log("API running on port 8080"))
 
 app.get('/', (req, res) => res.json('My API is running!'))
 
